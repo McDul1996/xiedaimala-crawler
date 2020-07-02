e75d890dfed6fd76d18dbf1e81758cd5c8a3246d
McDul <907420318@qq.com>
2020-07-02 14:35:14 +0800

    设计并实现基本的算法 (#2)
     设计并实现基本的算法
    
    这个提交中，我们实现了基本的爬虫算法
    虽然它还非常原始，但是我们想小步提交。

--- src/main/java/com/github/hcsp/io/Main.java
+++ src/main/java/com/github/hcsp/io/Main.java
@@ -2,13 +2,63 @@ package com.github.hcsp.io;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class Main {
     private static String url = "https://news.sina.cn/";
+    private static List<String> linkPoor = new ArrayList<>();
+
     public static void main(String[] args) throws IOException {
-        Document document = Jsoup.connect(url).get();
-        System.out.println(document.toString());
+        Set<String> processedLinks = new HashSet<>();
+        linkPoor.add(url);
+        while (true) {
+            if (linkPoor.isEmpty()) {
+                break;
+            }
+            String link = linkPoor.remove(linkPoor.size() - 1);
+//            System.out.println(link);
+            if (processedLinks.contains(link)) {
+                continue;
+            }
+            processedLinks.add(link);
+            if (isInterestingLink(link)) {
+                Document doc = httpGetAndParseHtml(link);
+                doc.select("a").stream().map(aTag->aTag.attr("href")).forEach(linkPoor::add);
+
+                storeIntoDatabaseIfItIsNewsPage(doc);
+            }
+        }
+    }
+    private static void storeIntoDatabaseIfItIsNewsPage(Document doc) {
+        ArrayList<Element> articleTag = doc.select("h1");
+        if (!articleTag.isEmpty()) {
+            doc.select("h1").stream().map(tag->articleTag.get(0).text()).forEach(System.out::println);
+        }
+    }
+
+    private static Document httpGetAndParseHtml(String link) throws IOException {
+        return Jsoup.connect(link).get();
+    }
+
+    private static boolean isInterestingLink(String link) {
+        return isIndexPage(link) && isNewsPage(link) && isNotLoginPage(link);
+    }
+
+    private static boolean isIndexPage(String link) {
+        return link.contains("sina.cn");
+    }
+
+    private static boolean isNewsPage(String link) {
+        return link.contains("news.sina.cn");
+    }
+
+    private static boolean isNotLoginPage(String link) {
+        return !link.contains("passport.sina.cn");
     }
 }
